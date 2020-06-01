62d9979ff02c4536ff4775ae12a68ca458d6bfe4
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2020-06-01 23:04:56 +0900

    따라하기
    ..

--- STL/11week/day2/review2.cpp
+++ STL/11week/day2/review2.cpp
@@ -1,115 +1,226 @@
+//#include <iostream>
+//#include <vector>
+//#include <random>
+//#include <set>
+//#include <unordered_set>
+//#include <chrono>
+//#include <algorithm>
+//using namespace std;
+//
+//const int NUM = 10000000;
+//const int TNUM = 100;
+//
+//default_random_engine dre;
+//normal_distribution<> nd(0, 1.0);
+//
+//size_t equalCnt = 0;
+//size_t lessCnt = 0;
+//class Test
+//{
+//	int n;
+//public:
+//	Test() = default;
+//	Test(int n) :n{ n } {}
+//	int GetNum() const { return n; };
+//
+//	bool operator==(const Test& rhs) const {
+//		equalCnt++;
+//		return n == rhs.n;
+//	}
+//	bool operator<(const Test& rhs) const {
+//		lessCnt++;
+//		return n < rhs.n;
+//	}
+//
+//};
+//
+//template<>
+//struct hash<Test>
+//{
+//	size_t operator()(const Test& rhs) const {
+//		return hash<int>()(rhs.GetNum());
+//	}
+//};
+//
+//
+//Test tnums[TNUM];
+//
+//
+//int main()
+//{
+//
+//	uniform_int_distribution<> uid(0, NUM);
+//
+//	for (Test& t : tnums)
+//		t = uid(dre);
+//
+//
+//
+//	vector<Test> v;
+//	v.reserve(NUM);
+//	double d{};
+//	for (int i = 0; i < NUM; ++i) {
+//		d = nd(dre);
+//		d = clamp(d, -5.0, 5.0);
+//		d += 5;
+//		d *= NUM / 10;
+//		v.emplace_back(d);
+//	}
+//
+//	multiset<Test> s{ v.begin(),v.end() };
+//	unordered_multiset<Test> us{ v.begin(),v.end() };
+//
+//	{
+//		cout << endl;
+//		equalCnt = 0;
+//		lessCnt = 0;
+//		cout << "벡터 찾기\n";
+//		auto start = chrono::steady_clock::now();
+//		for (const auto& t : tnums)
+//			find(v.begin(), v.end(), t);
+//		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림\n";
+//		cout << "== 횟수: " << equalCnt << endl;
+//		cout << "< 횟수: " << lessCnt << endl;
+//
+//	}
+//
+//	{
+//		cout << endl;
+//		equalCnt = 0;
+//		lessCnt = 0;
+//		cout << "멀티셋 찾기\n";
+//		auto start = chrono::steady_clock::now();
+//		for (const auto& t : tnums)
+//			s.find(t);
+//		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림\n";
+//		cout << "== 횟수: " << equalCnt << endl;
+//		cout << "< 횟수: " << lessCnt << endl;
+//
+//	}
+//	{
+//		cout << endl;
+//		equalCnt = 0;
+//		lessCnt = 0;
+//		cout << "언오더드멀티셋 찾기\n";
+//		auto start = chrono::steady_clock::now();
+//		for (const auto& t : tnums)
+//			us.find(t);
+//		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림\n";
+//		cout << "== 횟수: " << equalCnt << endl;
+//		cout << "< 횟수: " << lessCnt << endl;
+//
+//	}
+//
+//
+//}
+
+
 #include <iostream>
-#include <vector>
-#include <random>
 #include <set>
+#include <vector>
 #include <unordered_set>
-#include <chrono>
 #include <algorithm>
+#include <random>
+#include <chrono>
 using namespace std;
 
-const int NUM = 10000000;
-const int TNUM = 100;
+default_random_engine dre ;
+normal_distribution<> nd(0.0, 1.0);
 
-default_random_engine dre;
-normal_distribution<> nd(0, 1.0);
+const int NUM = 100'0;
+const int TNUM = 10;
 
-size_t equalCnt = 0;
-size_t lessCnt = 0;
-class Test
-{
-	int n;
+
+class Test {
+	int num;
+	static int ex;
 public:
+	static long long lessCnt;
+	static long long equalCnt;
 	Test() = default;
-	Test(int n) :n{ n } {}
-	int GetNum() const { return n; };
+	Test(int n) :num{ n } {}
+	int GetNum() const { return num; }
 
-	bool operator==(const Test& rhs) const {
+	bool operator==(const Test& rhs)const {
 		equalCnt++;
-		return n == rhs.n;
+		return num == rhs.num;
 	}
-	bool operator<(const Test& rhs) const {
+	bool operator<(const Test& rhs)const {
 		lessCnt++;
-		return n < rhs.n;
+		return num < rhs.num;
 	}
-
 };
 
+long long Test::equalCnt = 0;
+long long Test::lessCnt = 0;
 template<>
 struct hash<Test>
 {
-	size_t operator()(const Test& rhs) const {
+	size_t operator()(const Test& rhs)const {
 		return hash<int>()(rhs.GetNum());
 	}
 };
 
-
 Test tnums[TNUM];
-
 
 int main()
 {
-
-	uniform_int_distribution<> uid(0, NUM);
-
-	for (Test& t : tnums)
-		t = uid(dre);
-
-
-
 	vector<Test> v;
 	v.reserve(NUM);
 	double d{};
 	for (int i = 0; i < NUM; ++i) {
 		d = nd(dre);
 		d = clamp(d, -5.0, 5.0);
-		d += 5;
+		d += 5.0;
 		d *= NUM / 10;
 		v.emplace_back(d);
 	}
-
 	multiset<Test> s{ v.begin(),v.end() };
 	unordered_multiset<Test> us{ v.begin(),v.end() };
 
-	{
-		cout << endl;
-		equalCnt = 0;
-		lessCnt = 0;
-		cout << "벡터 찾기\n";
-		auto start = chrono::steady_clock::now();
-		for (const auto& t : tnums)
-			find(v.begin(), v.end(), t);
-		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림\n";
-		cout << "== 횟수: " << equalCnt << endl;
-		cout << "< 횟수: " << lessCnt << endl;
+	uniform_int_distribution<> uid(0, NUM-1);
 
+	for (int i = 0; i < TNUM; ++i)
+		tnums[i] = v[uid(dre)];
+	cout << "벡터의 개수는: " << v.size() << endl;
+	cout << "셋의 개수: " << s.size() << endl;
+	cout << "언오더드 셋의 개수: " << us.size() << endl;
+
+
+	{//벡터 찾기
+		cout << "\n벡터 찾기" << endl;
+		Test::equalCnt = 0;
+		Test::lessCnt = 0;
+		chrono::steady_clock::time_point start = chrono::steady_clock::now();
+		for (int i = 0; i < TNUM; ++i)
+			find(v.begin(), v.end(), tnums[i]);
+		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count()<<"밀리초 걸림" << endl;
+		cout << "==횟수: " << Test::equalCnt << endl;
+		
+		cout << "<횟수: " << Test::lessCnt << endl;
 	}
 
 	{
-		cout << endl;
-		equalCnt = 0;
-		lessCnt = 0;
-		cout << "멀티셋 찾기\n";
+		cout << "\n멀티셋 찾기" << endl;
+		Test::equalCnt = 0;
+		Test::lessCnt = 0;
 		auto start = chrono::steady_clock::now();
-		for (const auto& t : tnums)
+		for (const Test& t : tnums)
 			s.find(t);
-		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림\n";
-		cout << "== 횟수: " << equalCnt << endl;
-		cout << "< 횟수: " << lessCnt << endl;
-
+		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림" << endl;
+		cout << "==횟수: " << Test::equalCnt << endl;
+		cout << "<횟수: " << Test::lessCnt << endl;
 	}
+
 	{
-		cout << endl;
-		equalCnt = 0;
-		lessCnt = 0;
-		cout << "언오더드멀티셋 찾기\n";
+		cout << "\n언오더더드 멀티셋 찾기" << endl;
+		Test::equalCnt = 0;
+		Test::lessCnt = 0;
 		auto start = chrono::steady_clock::now();
-		for (const auto& t : tnums)
+		for (const Test& t : tnums)
 			us.find(t);
-		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림\n";
-		cout << "== 횟수: " << equalCnt << endl;
-		cout << "< 횟수: " << lessCnt << endl;
-
+		cout << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << "밀리초 걸림" << endl;
+		cout << "==횟수: " << Test::equalCnt << endl;
+		cout << "<횟수: " << Test::lessCnt << endl;
 	}
-
-
-}
+} 
--- new 3D/LabProjects/LabProject03_1/Timer.cpp
+++ new 3D/LabProjects/LabProject03_1/Timer.cpp
@@ -12,17 +12,21 @@
 명은 윈도우 API 도움말을 참조하라.*/
 CGameTimer::CGameTimer()
 {
-	::QueryPerformanceFrequency((LARGE_INTEGER
-		*)&m_PerformanceFrequencyPerSec);
-	::QueryPerformanceCounter((LARGE_INTEGER
-		*)&m_nLastPerformanceCounter);
-	m_fTimeScale = 1.0 / (double)m_PerformanceFrequencyPerSec;
-	m_nBasePerformanceCounter = m_nLastPerformanceCounter;
-	m_nPausedPerformanceCounter = 0;
-	m_nStopPerformanceCounter = 0;
+	if (::QueryPerformanceFrequency((LARGE_INTEGER*)&m_nPerformanceFrequency))
+	{
+		m_bHardwareHasPerformanceCounter = TRUE;
+		::QueryPerformanceCounter((LARGE_INTEGER*)&m_nLastTime);
+		m_fTimeScale = 1.0f / m_nPerformanceFrequency;
+	}
+	else
+	{
+		m_bHardwareHasPerformanceCounter = FALSE;
+		m_nLastTime = ::timeGetTime();
+		m_fTimeScale = 0.001f;
+	}
 	m_nSampleCount = 0;
 	m_nCurrentFrameRate = 0;
-	m_FramePerSecond = 0;
+	m_nFramesPerSecond = 0;
 	m_fFPSTimeElapsed = 0.0f;
 }
 CGameTimer::~CGameTimer()
@@ -31,63 +35,67 @@ CGameTimer::~CGameTimer()
 
 void CGameTimer::Tick(float fLockFPS)
 {
-	float fTimeElapsed;
-	::QueryPerformanceCounter((LARGE_INTEGER
-		*)&m_nCurrentPerformanceCounter);
-	//이 함수를 마지막으로 호출한 이후 경과한 시간을 계산한다.
-	fTimeElapsed = float((m_nCurrentPerformanceCounter -
-		m_nLastPerformanceCounter) * m_fTimeScale);
-	//이 함수의 파라메터(fLockFPS)가 0보다 크면 이 시간만큼 호출한 함수를 기다리게 한다.
-	if (fLockFPS > 0.0f)
+	if (m_bHardwareHasPerformanceCounter)
 	{
+		::QueryPerformanceCounter((LARGE_INTEGER*)&m_nCurrentTime);
+	}
+	else
+	{
+		m_nCurrentTime = ::timeGetTime();
+	}
+	//마지막으로 이 함수를 호출한 이후 경과한 시간을 계산한다
+	float fTimeElapsed = (m_nCurrentTime - m_nLastTime) * m_fTimeScale;	if (fLockFPS > 0.0f)
+	{
+		//이 함수의 파라메터(fLockFPS)가 0보다 크면 이 시간만큼 호출한 함수를 기다리게 한다.
 		while (fTimeElapsed < (1.0f / fLockFPS))
 		{
-			::QueryPerformanceCounter((LARGE_INTEGER
-				*)&m_nCurrentPerformanceCounter);
-			//이 함수를 마지막으로 호출한 이후 경과한 시간을 계산한다.
-			fTimeElapsed = float((m_nCurrentPerformanceCounter -
-				m_nLastPerformanceCounter) * m_fTimeScale);
+			if (m_bHardwareHasPerformanceCounter)
+			{
+				::QueryPerformanceCounter((LARGE_INTEGER*)&m_nCurrentTime);
+			}
+			else
+			{
+				m_nCurrentTime = ::timeGetTime();
+			}
+			//마지막으로 이 함수를 호출한 이후 경과한 시간을 계산한다.
+			fTimeElapsed = (m_nCurrentTime - m_nLastTime) * m_fTimeScale;
 		}
 	}
-	//현재 시간을 m_nLastTime에 저장한다.
-	m_nLastPerformanceCounter = m_nCurrentPerformanceCounter;
+	//현재 시간을 m_nLastTime에 저장한다
+	m_nLastTime = m_nCurrentTime;
 	/* 마지막 프레임 처리 시간과 현재 프레임 처리 시간의 차이가 1초보다 작으면 현재 프레임 처리 시간
 	을 m_fFrameTime[0]에 저장한다. */
 	if (fabsf(fTimeElapsed - m_fTimeElapsed) < 1.0f)
 	{
-		::memmove(&m_fFrameTime[1], m_fFrameTime, (MAX_SAMPLE_COUNT - 1) *
-			sizeof(float));
+		::memmove(&m_fFrameTime[1], m_fFrameTime, (MAX_SAMPLE_COUNT - 1) * sizeof(float));
 		m_fFrameTime[0] = fTimeElapsed;
 		if (m_nSampleCount < MAX_SAMPLE_COUNT) m_nSampleCount++;
 	}
 	//초당 프레임 수를 1 증가시키고 현재 프레임 처리 시간을 누적하여 저장한다.
-	m_FramePerSecond++;
+	m_nFramesPerSecond++;
 	m_fFPSTimeElapsed += fTimeElapsed;
 	if (m_fFPSTimeElapsed > 1.0f)
 	{
-		m_nCurrentFrameRate = m_FramePerSecond;
-		m_FramePerSecond = 0;
+		m_nCurrentFrameRate = m_nFramesPerSecond;
+		m_nFramesPerSecond = 0;
 		m_fFPSTimeElapsed = 0.0f;
 	}
 	//누적된 프레임 처리 시간의 평균을 구하여 프레임 처리 시간을 구한다.
 	m_fTimeElapsed = 0.0f;
-	for (ULONG i = 0; i < m_nSampleCount; i++) m_fTimeElapsed +=
-		m_fFrameTime[i];
-	if (m_nSampleCount > 0) m_fTimeElapsed /= m_nSampleCount;
-}
-
-unsigned long CGameTimer::GetFrameRate(LPTSTR lpszString, int
-	nCharacters)
+	for (ULONG i = 0; i < m_nSampleCount; i++) m_fTimeElapsed += m_fFrameTime[i];
+	if (m_nSampleCount > 0)
+		m_fTimeElapsed /= m_nSampleCount;
+}
+unsigned long CGameTimer::GetFrameRate(LPTSTR lpszString, int nCharacters)
 {
 	if (lpszString)
 	{
-		//현재 프레임 레이트를 문자열로 변환하여 lpszString 버퍼에 쓰고 “ FPS”와 결합한다.
-		::_itow_s(m_nCurrentFrameRate, lpszString, nCharacters, 10);
-		::wcscat_s(lpszString, nCharacters, _T(" FPS)"));
+		_itow_s(m_nCurrentFrameRate, lpszString, nCharacters, 10);
+		wcscat_s(lpszString, nCharacters, _T(" FPS)"));
 	}
 	return(m_nCurrentFrameRate);
 }
 float CGameTimer::GetTimeElapsed()
 {
 	return(m_fTimeElapsed);
-}
+}
