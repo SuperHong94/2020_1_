2f2cc6ce3ece5ba6299bf26caf5523db2ab245ec
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2020-06-28 22:04:16 +0900

    도리짓고땡 완성 ㄷㄷ

--- 도리짓고땡/Board.py
+++ 도리짓고땡/Board.py
@@ -3,7 +3,7 @@ from tkinter import font
 import Player
 import Card
 import random
-
+from winsound import *
 class MainGUI:
     def SetupLabel(self):
         for i in range(len(self.players)):
@@ -15,8 +15,20 @@ class MainGUI:
         self.totalMoneyL = Label(text=str(self.totalMoney) + "만", width=6, height=1, font=self.fontstyle,
                                   bg="green", fg="yellow")
         self.totalMoneyL.place(x=690, y=500)
+        for i in range(7): #플레이어 하나당 7개의 라벨을 추가 마지막은 메이드와 족보 결과 마지막은 승리결과
+            self.LDealerTypes.append(Label(self.window,bg='green',font=self.fontstyle,fg='white',
+                      text="")) #딜러 라벨 추가
+            for j in range(len(self.players)):
+                self.LPlayerTypes[j].append( Label(self.window,bg='green',font=self.fontstyle,fg='white',
+                      text=""))
 
+         #이미지 리스트
+        for i in range(5):  # 이미지 라벨 리스트에 카드 5장 채우기 반복문을 나눈것은 배치의 개념
+            self.LDealerCards.append(Label(self.window, image=self.delarImage))
+            for j in range(len(self.players)):
+                self.LPlayerCards[j].append(Label(self.window, image=""))
     def BetMoney(self,num,value):
+        PlaySound('Resource/sounds/chip.wav', SND_FILENAME)
         if self.totalMoney>=1:
             if value==5 and self.totalMoney>=value:
                 self.totalMoney-=value
@@ -33,32 +45,162 @@ class MainGUI:
 
     def drawCards(self):
         if self.turn==1:
+            PlaySound('Resource/sounds/cardFlip1.wav', SND_FILENAME)
             self.LDealerCards[0].place(x=100,y=100)
             #플레이어 카드
             for i in range(len(self.players)):
                 self.LPlayerCards[i][0].place(x=30+(i*215),y=380)
+                self.LPlayerTypes[i][0]['text']=str(self.players[i].cards[0].type)
+                self.LPlayerTypes[i][0].place(x=57+(i*215),y=350)
 
         elif self.turn==2:
+            PlaySound('Resource/sounds/cardFlip1.wav', SND_FILENAME)
             for i in range(1,4): #카드 3장 그리기
                 self.LDealerCards[i].place(x=100+(i*30), y=100)
                 for j in range(len(self.players)):
                     self.LPlayerCards[j][i].place(x=30+(j*215)+(i*30),y=380)
+                    self.LPlayerTypes[j][i]['text']=str(self.players[j].cards[i].type)
+                    self.LPlayerTypes[j][i].place(x=57 + (j * 215)+(i*30), y=350)
+
 
         elif self.turn==3:
-            self.LDealerCards.append(Label(self.window, image=self.delarImage))
+            PlaySound('Resource/sounds/cardFlip1.wav', SND_FILENAME)
+            for i in range(len(self.LDealerCards)):
+                self.LDealerCards[i]['image']= self.dealer.cards[i].cImage
+                self.LDealerTypes[i]['text'] = str(self.dealer.cards[i].type)
+                self.LDealerTypes[i].place(x=127+(i*30),y=70)
             self.LDealerCards[4].place(x=220, y=100)
+
             # 플레이어 카드
             for i in range(len(self.players)):
                 self.LPlayerCards[i][4].place(x=30 + (i * 215)+120, y=380)
+                self.LPlayerTypes[i][4]['text']=str(self.players[i].cards[4].type)
+                self.LPlayerTypes[i][4].place(x=57+ (i * 215)+120, y=350)
+
+            #승리자 체크
+            self.CheckWinnner()
+
+
+
+
 
 
 
     def CheckWinnner(self):
+        winCnt=0
         for i in range(len(self.players)):
             self.Button5[i]['state'] = 'disable'
             self.Button5[i]['bg'] = 'gray'
             self.Button1[i]['state'] = 'disable'
             self.Button1[i]['bg'] = 'gray'
+        self.AgainButton['state']='active'
+        self.AgainButton['bg']='white'
+
+        #족보 결과 추가
+            #메이드 결과
+        self.dealer.judgement()
+        if self.dealer.madeLst == None:
+            self.LDealerTypes[5]['text'] = "노메이드"
+
+            self.LDealerTypes[5]['fg'] = "blue"
+            self.LDealerTypes[5].place(x=127, y=40)
+        else:
+            self.LDealerTypes[5]['text'] = self.dealer.madeLst[3]+" "+self.dealer.rScore[1]
+            self.LDealerTypes[5]['fg'] = "blue"
+            self.LDealerTypes[5].place(x=127, y=40)
+
+            numLst = list(self.dealer.madeLst[:5]) #중복피하려고
+            for i in range(5): #메이드된거 색깔바꾸기
+                if (int(self.LDealerTypes[i]['text'])in self.dealer.madeLst) and (int(self.LDealerTypes[i]['text'])in numLst):
+                    numLst.remove(int(self.LDealerTypes[i]['text']))
+                    self.LDealerTypes[i]['fg']='yellow'
+                    self.LDealerCards[i].place(x=100+(i*30), y=110)
+
+        for i in range(len(self.players)): #"플레이어 족보결과"
+            self.players[i].judgement()
+            if self.players[i].madeLst==None:
+                self.LPlayerTypes[i][5]['text']="노메이드"
+                self.LPlayerTypes[i][5]['fg']="blue"
+                self.LPlayerTypes[i][5].place(x=50+(i*215),y=320)
+            else:
+                self.LPlayerTypes[i][5]['text'] = self.players[i].madeLst[3]+" "+self.players[i].rScore[1]
+                self.LPlayerTypes[i][5]['fg'] = "blue"
+                self.LPlayerTypes[i][5].place(x=50 + (i * 215), y=320)
+
+                numLst = list(self.players[i].madeLst[:5])
+                for j in range(5):  # 메이드된거 색깔바꾸기
+                    if (int(self.LPlayerTypes[i][j]['text']) in self.players[i].madeLst) \
+                            and (int(self.LPlayerTypes[i][j]['text']) in numLst):
+                        numLst.remove(int(self.LPlayerTypes[i][j]['text']))
+                        self.LPlayerTypes[i][j]['fg'] = 'yellow'
+                        self.LPlayerCards[i][j].place(x=30+(i*215)+(j*30),y=390)
+
+        for i in range(len(self.players)):
+            if self.dealer.madeLst==None:
+                if self.players[i].madeLst!=None:
+                    self.LPlayerTypes[i][6]['text']="승리"
+                    self.LPlayerTypes[i][6]['fg']="red"
+                    self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+                    self.totalMoney += self.players[i].money * 2
+                    self.totalMoneyL['text']=str(self.totalMoney)+"만"
+                    winCnt+=1
+
+                else: #둘다 노메이드일때
+                    self.LPlayerTypes[i][6]['text'] = "무승부"
+                    self.LPlayerTypes[i][6]['fg'] = "red"
+                    self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+
+            else:
+                if self.players[i].madeLst == None:
+                    self.LPlayerTypes[i][6]['text'] = "패배"
+                    self.LPlayerTypes[i][6]['fg'] = "red"
+                    self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+
+                else:
+                    if self.dealer.rScore[0]>self.players[i].rScore[0]:
+                        self.LPlayerTypes[i][6]['text'] = "패배"
+                        self.LPlayerTypes[i][6]['fg'] = "red"
+                        self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+
+                    elif self.dealer.rScore[0]<self.players[i].rScore[0]:
+                        self.LPlayerTypes[i][6]['text'] = "승리"
+                        self.LPlayerTypes[i][6]['fg'] = "red"
+                        self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+                        self.totalMoney+=self.players[i].money*2
+                        self.totalMoneyL['text']=str(self.totalMoney)+"만"
+                        winCnt += 1
+
+                    else:#둘다 메이드인데 점수같을때
+                        if self.dealer.dan>self.players[i].dan: #단으로비교
+                            self.LPlayerTypes[i][6]['text'] = "패배"
+                            self.LPlayerTypes[i][6]['fg'] = "red"
+                            self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+                        elif self.dealer.dan<self.players[i].dan:
+                            self.LPlayerTypes[i][6]['text'] = "승리"
+                            self.LPlayerTypes[i][6]['fg'] = "red"
+                            self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+                            self.totalMoney += self.players[i].money * 2
+                            self.totalMoneyL['text'] = str(self.totalMoney) + "만"
+                            winCnt += 1
+                        else:
+                            self.LPlayerTypes[i][6]['text'] = "무승부"
+                            self.LPlayerTypes[i][6]['fg'] = "red"
+                            self.LPlayerTypes[i][6].place(x=50 + (i * 215), y=280)
+
+        if winCnt>0:
+            PlaySound('Resource/sounds/win.wav', SND_FILENAME)
+        else:
+            PlaySound('Resource/sounds/wrong.wav', SND_FILENAME)
+
+
+
+
+
+
+
+
+
+
 
     def GamePlay(self):
         if self.turn==1:  #첫번째턴
@@ -68,9 +210,9 @@ class MainGUI:
                 for p in self.players:
                     p.cards.append(Card.Card(self.CardeDeck.pop()))  # 카드리스트에서 한장빼서 플레이어에게 준다.
             for i in range(5): #이미지 라벨 리스트에 카드 5장 채우기 반복문을 나눈것은 배치의 개념
-                self.LDealerCards.append(Label(self.window, image=self.delarImage))
+                self.LDealerCards[i]['image']=self.delarImage
                 for j in range(len(self.players)):
-                    self.LPlayerCards[j].append(Label(self.window,image=self.players[j].cards[i].cImage))
+                    self.LPlayerCards[j][i]['image']=self.players[j].cards[i].cImage
 
 
         self.drawCards()
@@ -78,25 +220,51 @@ class MainGUI:
 
 
     def Deal(self):
-        print(self.turn,"deal버튼")
-
         self.DealButton['state'] = 'disable'
         self.DealButton['bg'] = 'gray'
-        self.AgainButton['state'] = 'active'
-        self.AgainButton['bg']='white'
         self.turn += 1  # 턴증가
         self.GamePlay() #게임시작
 
         pass
     def Again(self):
+        PlaySound('Resource/sounds/ding.wav', SND_FILENAME)
         self.turn=0
         self.AgainButton['state'] = 'disable'
         self.AgainButton['bg'] = 'gray'
+
+        for i in range(7):  # 라벨들 안보이는곳으로 보내버리기
+            self.LDealerTypes[i]['fg']='white'
+            self.LDealerTypes[i].place(x=1000,y=1000)
+            for j in range(len(self.players)):
+                self.LPlayerTypes[j][i]['fg']='white'
+                self.LPlayerTypes[j][i].place(x=1000,y=1000)
+
+        for i in range(5):  # 이미지안보이는곳으로 보내버리기
+            self.LDealerCards[i].place(x=1000,y=1000)
+            for j in range(len(self.players)):
+                self.LPlayerCards[j][i].place(x=1000,y=1000)
+        #딜러 초기화
+        self.dealer.cards.clear()
+        if self.dealer.madeLst!=None:
+            self.dealer.madeLst.clear()
+        self.dealer.results.clear()
+        self.dealer.typeLst.clear()
+        self.dealer.rScore = None
+        self.dealer.dan=0
         for i in range(len(self.players)):
             self.Button5[i]['state']='active'
             self.Button5[i]['bg']='white'
             self.Button1[i]['state']='active'
             self.Button1[i]['bg']='white'
+            self.players[i].cards.clear()
+            if self.players[i].madeLst!=None:
+                self.players[i].madeLst.clear()
+            self.players[i].results.clear()
+            self.players[i].typeLst.clear()
+            self.players[i].rScore=None
+            self.players[i].money=0
+            self.players[i].dan=0
+            self.PlayerMoneyL[i]['text']=str(self.players[i].money)+"만"
         pass
     def SetupButton(self):
         width=220
@@ -138,7 +306,7 @@ class MainGUI:
 
     def __init__(self):
         self.window=Tk()
-        self.window.title("도리짓고땡")
+        self.window.title("도리짓고땡_2018182041_홍순조")
         self.window.geometry("800x600")
         self.fontstyle = font.Font(self.window, size=15, weight='bold', family='Consolas')
 
@@ -157,9 +325,11 @@ class MainGUI:
         self.PlayerMoneyL=[]
         self.turn=0
         self.CardeDeck=[]
-        self.LPlayerCards=[[],[],[]]#플레이어 3명
+        self.LPlayerCards=[[],[],[]]#플레이어 3명 이미지
         self.LDealerCards=[]
         self.delarImage=p=PhotoImage(file=self.fileRoute+"cardback.gif")
+        self.LPlayerTypes=[[],[],[]] # 플레이어 타입리스트 [x][5]번째에 족보 결과 나타낼꺼임
+        self.LDealerTypes=[] # 딜러 타입리스트 5번째에 족보결과
         #기본 라벨 셋업
         self.SetupLabel()
         self.SetupButton()
--- 도리짓고땡/Player.py
+++ 도리짓고땡/Player.py
@@ -1,4 +1,129 @@
+
+
+
 class Player:
     def __init__(self):
         self.money=0
         self.cards=[]
+        self.madeLst=[]
+        self.typeLst=[] #플레이어 카드들의 타입(1월~12월)들을 넣어준다.
+        self.results=[]# 족보비교 카드 인덱스 리스트
+        self.rScore=None #튜플로 점수와하고 이름 넘겨주자.
+        self.dan=0
+
+    def IsMade(self):
+        if (8 in self.typeLst) and(self.typeLst.count(1)>=2): #콩콩팔
+            self.dan = 1
+            return [1,1,8,"콩콩팔(1 1 8)"]
+        elif (1 in self.typeLst) and (2 in self.typeLst) and (7 in self.typeLst) : #삐리칠
+            self.dan = 1
+            return [1,2,7,"삐리칠(1 2 7)"]
+        elif (1 in self.typeLst) and (3 in self.typeLst) and (6 in self.typeLst) : #물삼육
+            self.dan = 1
+            return [1,3,6,"물삼육(1 3 6)"]
+        elif (1 in self.typeLst) and (4 in self.typeLst) and (5 in self.typeLst): #빽새오
+            self.dan = 1
+            return [1, 4, 5,"빽새오(1 4 5)"]
+        elif (1 in self.typeLst) and (9 in self.typeLst) and (10 in self.typeLst): #삥구장
+            self.dan = 1
+            return [1, 9, 10,"삥구장(1 9 10)"]
+        elif(6 in self.typeLst) and (self.typeLst.count(2)>=2):  # 니니육
+            self.dan = 2
+            return [2, 2, 6,"니니육(2 2 6)"]
+        elif (2 in self.typeLst) and (3 in self.typeLst) and (5 in self.typeLst):  # 이삼오
+            self.dan = 2
+            return [2, 3, 5,"이삼오(2 3 5)"]
+        elif (2 in self.typeLst) and (8 in self.typeLst) and (10 in self.typeLst):  # 이판장
+            self.dan = 2
+            return [2, 8, 10,"이판장(2 8 10)"]
+        elif  (self.typeLst.count(3)>=2)and (4 in self.typeLst):  # 심심새
+            self.dan = 3
+            return [3, 3, 4,"심심새(3 3 4)"]
+
+        elif (3 in self.typeLst) and (7 in self.typeLst) and (10 in self.typeLst):  # 삼칠장
+            self.dan = 3
+            return [3, 7, 10,"삼칠장(3 7 10)"]
+        elif (3 in self.typeLst) and (8 in self.typeLst) and (9 in self.typeLst):  # 삼빡구
+            self.dan = 3
+            return [3, 8, 9,"삼빡구(3 8 9)"]
+        elif  (self.typeLst.count(4) >= 2)and (2 in self.typeLst):  # 살살이
+            self.dan = 4
+            return [4, 4, 2,"살살이(4 4 2)"]
+        elif (4 in self.typeLst) and (6 in self.typeLst) and (10 in self.typeLst):  # 사륙장
+            self.dan = 4
+            return [4, 6, 10,"사륙장(4 6 10)"]
+        elif (4 in self.typeLst) and (7 in self.typeLst) and (9 in self.typeLst):  # 사칠구
+            self.dan = 4
+            return [4, 7, 9,"사칠구(4 7 9)"]
+        elif (self.typeLst.count(5) >= 2) and (10 in self.typeLst):  # 꼬꼬장
+            self.dan = 5
+            return [5, 5, 10,"꼬꼬장(5 5 10)"]
+        elif (5 in self.typeLst) and (6 in self.typeLst) and (9 in self.typeLst):  # 오륙구
+            self.dan = 5
+            return [5, 6, 9,"오륙구(5 6 9)"]
+        elif (5 in self.typeLst) and (7 in self.typeLst) and (8 in self.typeLst):  # 오리발
+            self.dan = 5
+            return [5, 7, 8,"오리발(5 8 8)"]
+        elif (self.typeLst.count(6) >= 2) and (8 in self.typeLst):  # 쭉쭉팔
+            self.dan = 6
+            return [6, 6, 8,"쭉쭉팔(6 6 8)"]
+        elif (self.typeLst.count(7) >= 2) and (6 in self.typeLst):  # 철철육
+            self.dan = 7
+            return [7, 7, 6,"철철육(7 7 6)"]
+        elif (self.typeLst.count(8) >= 2) and (4 in self.typeLst):  # 팍팍싸
+            self.dan = 8
+            return [8, 8, 4,"팍팍싸(8 8 4)"]
+        elif (self.typeLst.count(9) >= 2) and (2 in self.typeLst):  # 구구리
+            self.dan = 9
+            return [9, 9, 2,"구구리(9 9 2)"]
+        else:
+            return None
+
+
+    def defineResult(self):
+        if (300 in self.results) and (800 in self.results):
+            return (3800,"삼팔광땡") #38광땡이 최고점수
+        elif(100 in self.results) and (800 in self.results):
+            return (1800,"일팔광땡") #18
+        elif (100 in self.results) and (300 in self.results):
+            return (1300,"일삼광땡")  # 13광땡이
+
+        for i in range(2): #광땡아니면 광땡 점수 제거
+            self.results[i]%=100
+
+        if len(set(self.results))==1: #n땡
+            if self.results[0]==1:
+                return (self.results[0] * 100,"삥땡")  # 점수는 n*100점
+            elif self.results[0]==10:
+                return (self.results[0] * 100, "장땡")  # 점수는 n*100점
+            else:
+                return (self.results[0] * 100, str(self.results[0])+"땡")  # 점수는 n*100점
+
+        else: #끗
+            temp=self.results[0]+self.results[1]
+            if temp%10==0:
+                return (temp%10,"망통")
+            else:
+                return (temp%10,str(temp%10)+"끗")
+
+
+
+
+    def judgement(self):
+        for card in self.cards: #카드의 type을 type리스트에 추가
+            self.typeLst.append(card.type)
+        self.madeLst=self.IsMade()
+
+        if self.madeLst!=None:
+            tempLst=self.madeLst[:3]
+            for i in range(5):
+                if not(self.cards[i].type in tempLst):
+                    if (self.cards[i].num==1)and(self.cards[i].type==1 or self.cards[i].type==3 or self.cards[i].type==8):
+                        self.results.append(self.cards[i].type*100) #광이면 100 곱한다. ex) 1광==100 3광==300 8광==800
+                    else:
+                        self.results.append(self.cards[i].type)
+                else: #중복해서 있지않게하기위해서 ex)
+                    tempLst.remove(self.cards[i].type)
+
+            self.rScore=self.defineResult()
+
